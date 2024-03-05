60705984d06077f1d9139c6d4e17b2ed68398902
seblessa <sebastiaolessa2002@outlook.com>
2024-03-05 16:25:39 +0000

    update

--- controllers/TP1/ex1/main.py
+++ controllers/TP1/ex1/main.py
@@ -9,5 +9,6 @@ from controllers.utils import cmd_vel
 # Create the Robot instance.
 robot: Robot = Robot()
 
-cmd_vel(robot, 0.1, 0)
-robot.step(1000)  # 1000 ms
+while robot.step() != -1:
+    cmd_vel(robot, 0.1, 0)
+
